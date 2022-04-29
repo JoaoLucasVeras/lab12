9fd549c9e071e0e1e0176b35d8577f05678a8c9c
JoaoV <joao.lucas.veras9@gmail.com>
2022-04-29 16:22:17 -0700

    updated TreeNode class

--- src/trees/FamilyTree.java
+++ src/trees/FamilyTree.java
@@ -31,6 +31,8 @@ public class FamilyTree
         
         void addChild(TreeNode childNode)
         {
+        	children.add(childNode);
+        	childNode.parent = this;
             // Add childNode to this node's children list. Also
             // set childNode's parent to this node.
         }
@@ -59,14 +61,19 @@ public class FamilyTree
         }
         
         
-        // Returns a list of ancestors of this TreeNode, starting with this node’s parent and
+        // Returns a list of ancestors of this TreeNode, starting with this  parent and
         // ending with the root. Order is from recent to ancient.
         ArrayList<TreeNode> collectAncestorsToList()
         {
             ArrayList<TreeNode> ancestors = new ArrayList<>();
-
+            
+            for(TreeNode child: children) {
+            	if(child.parent.parent != null) {
+            		ancestors.add(child.parent.parent);
+            	}
+            }
             // ?????  Collect ancestors of this TreeNode into the array list. HINT: going up
-            // the nodes of a tree is like traversing a linked list. If that isn’t clear,
+            // the nodes of a tree is like traversing a linked list. If that  clear,
             // draw a tree, mark any leaf node, and then mark its ancestors in order from
             // recent to ancient. Expect a question about this on the final exam.
 
@@ -112,8 +119,8 @@ public class FamilyTree
 
 		// Parse the input file. Create a FileReader that reads treeFile. Create a BufferedReader
 		// that reads from the FileReader.
-		FileReader fr = ???
-		BufferedReader br = ???
+		FileReader fr = new FileReader(treeFile);
+		BufferedReader br = new BufferedReader(fr);
 		String line;
 		while ((line = br.readLine()) != null)
 			addLine(line);
@@ -129,14 +136,14 @@ public class FamilyTree
 	private void addLine(String line) throws TreeException
 	{
 		// Extract parent and array of children.
-		int colonIndex = ?? should be the index of the colon in line.
+		int colonIndex =  //should be the index of the colon in line.
 		if (colonIndex < 0)
-			?? throw a TreeException with a useful message
-		String parent = ?? The substring of line that starts at char #0 and ends just before colonIndex. Check the API for 
-				           class java.util.String, method substring(), if you need guidance.
-		String childrenString = ?? The substring of line that starts just after colonIndex and goes through the end of
-				                   the line. You'll use a different version of substring().
-		String[] childrenArray = ?? Call childrenString.split(). Check the API for details. The result will be an array
+			// throw a TreeException with a useful message
+		//String parent = ?? The substring of line that starts at char #0 and ends just before colonIndex. Check the API for 
+			//	           class java.util.String, method substring(), if you need guidance.
+		//String childrenString = ?? The substring of line that starts just after colonIndex and goes through the end of
+			//	                   the line. You'll use a different version of substring().
+		//String[] childrenArray = ?? Call childrenString.split(). Check the API for details. The result will be an array
 				                    of strings, with the separating commas thrown away.
 		
 		// Find parent node. If root is null then the tree is empty and the
@@ -147,13 +154,13 @@ public class FamilyTree
 			parentNode = root = new TreeNode(parent);
 		else
 		{
-			parentNode = root.?????  There's a method in Node that searches for a named node. 
-			??? If the parent node wasn't found, there must have been something wrong in the 
-				data file. Throw an exception.
+			parentNode = root.//?????  //There's a method in Node that searches for a named node. 
+			//??? If the parent node wasn't found, there must have been something wrong in the 
+			//	data file. Throw an exception.
 		}
 		
 		// Add child nodes to parentNode.
-		?? For each name in childrenArray, create a new node and add that node to parentNode.
+		//?? For each name in childrenArray, create a new node and add that node to parentNode.
 	}
 	
 	
